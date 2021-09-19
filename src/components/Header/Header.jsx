16594ba96eed6fff9bd7d155fc3100579f56dbd7
aom19 <39848284+aom19@users.noreply.github.com>
2021-09-19 17:52:15 +0300

    set navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { AutoComplete  } from '@react-google-maps/api';
+import {AppBar , Toolbar , Typography , InputBase , Box} from '@material-ui/core' ;
+import SearchIcon from "@material-ui/icons/Search"
+
+import useStyles from './styles';
+
 
 const Header = () => {
+    //useStyles hook
+    const classes =useStyles();
     return (
-        <div>
-            
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant= "h5" className={classes.title}>
+                    Travel 
+                    </Typography>
+                    <Box display="flex">
+                    <Typography variant= "h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <AutoComplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}> 
+                                <SearchIcon />
+                            </div>
+                        </div>
+                        <InputBase placeholder ="Seaarch" classes = {{root: classes.inputRoot, input : classes.inputInput}} />
+
+                    {/* </AutoComplete>  */}
+                    </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PlaceDetails = () => {
     return (
         <div>
-            
+            <h2>PlaceDetails</h2>
         </div>
     )
 }
